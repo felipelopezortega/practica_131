2627780304c2d1ab8e8228f7119faf3e96618a4c
visita <visita@TELEMATICA.UCOL.MX>
2022-04-05 08:49:04 -0500

    updated

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express"); //importamos la dependencia
+var app = express () //declaramos una App de Express
+app.set("view engine", "ejs"); // Aquí se especifica a nuestra App que su template será EJS
+
+app.get("/", function(req, res){
+
+    
+})
